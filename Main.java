1fb1eedcd514eefe8dd2b7e1bbdda730262f0d00
jquie <jacquelynphan03@gmail.com>
2024-12-18 18:18:32 +0000

    done

--- Main.java
+++ Main.java
@@ -4,6 +4,10 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    // Problem 1
+    for (int i = 1; i <= 25; i+=2)
+    {
+      System.out.println(i);
+    }
   }
 }

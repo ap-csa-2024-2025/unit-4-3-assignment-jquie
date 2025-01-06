c52c0978bf5c9cd3b8e28ea64d2947878f00e8d0
jquie <jacquelynphan03@gmail.com>
2025-01-06 18:12:11 +0000

    done!

--- Main.java
+++ Main.java
@@ -9,5 +9,48 @@ public class Main
     {
       System.out.println(i);
     }
+    // Problem 2
+    for (int i = 17; i <= 73; i++)
+    {
+      System.out.print(i +  " ");
+      if (i % 10 == 6)
+      {
+        System.out.println();
+      }
+    }
+    // Problem 3
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a number between 0 and 50:");
+    int n = sc.nextInt();
+    if (!(0 <= n && n <= 50))
+    {
+      System.out.println("Error");
+    }
+    else
+    {
+      for (int i = n; i <= 50; i++)
+      {
+        System.out.print(i + " ");
+        if (i % 5 == (n + 4) % 5)
+        {
+          System.out.println();
+        }
+      }
+    }
+    // Problem 4
+    System.out.println("Enter a positive integer:");
+    n = sc.nextInt();
+    if (n > 0)
+    {
+      n -= (n % 3);	
+     for (int i = n; i >= 0; i -= 3)
+     {
+      System.out.print(i + " ");
+     }
+    }
+    else
+    {
+      System.out.println("error");
+    }
   }
 }
